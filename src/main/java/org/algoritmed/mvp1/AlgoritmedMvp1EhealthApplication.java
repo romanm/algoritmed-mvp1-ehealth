391caffa1464403313c7618e74722d65244fa273
roman.mishchenko@gmail.com <roman.mishchenko@gmail.com>
2017-02-27 12:11:58 +0200

    first commit - init project

---
+++ src/main/java/org/algoritmed/mvp1/AlgoritmedMvp1EhealthApplication.java
@@ -0,0 +1,12 @@
+package org.algoritmed.mvp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoritmedMvp1EhealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoritmedMvp1EhealthApplication.class, args);
+	}
+}
