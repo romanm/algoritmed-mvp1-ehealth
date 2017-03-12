6652cefb1c54db2e3dd5b9e0402b0b1c1cc411d7
roman.mishchenko@gmail.com <roman.mishchenko@gmail.com>
2017-03-13 01:56:58 +0200

    RC1 - ICPC2 navigation

--- src/main/resources/static/f/js/mvp1App-meddoc.js
+++ src/main/resources/static/f/js/mvp1App-meddoc.js
@@ -7,5 +7,26 @@
 		console.log('---mvp1App-----Mvp1Ctrl--------');
 		initAll($http, $scope);
 	});
+	
+
+	app.controller('AccordionCtrl', function ($scope) {
+		console.log('---AccordionCtrl------------');
+		this.expandItem = function (o){
+			o.expand = !o.expand;
+		}
+
+		this.expandAll = function (o, expandO){
+			console.log('---AccordionCtrl-----expandAll--------'+ o.expandAll);
+//			o.expandAll = !o.expandAll;
+			expandAll(o, expandO);
+			/*
+			angular.forEach(expandO, function(oToExpand, key) {
+				oToExpand.expand = o.expandAll;
+			});
+			 * */
+		}
+
+	});
+
 
 })(window.angular);
--- src/main/resources/static/f/js/mvp1InitAll-meddoc.js
+++ src/main/resources/static/f/js/mvp1InitAll-meddoc.js
@@ -1,3 +1,9 @@
+function expandAll (o, expandO){
+	angular.forEach(expandO, function(oToExpand, key) {
+		oToExpand.expand = o.expandAll;
+	});
+}
+
 function initAll ($http, $scope){
 	console.log('----initAll---------------');
 	initAllServer($http, $scope);
@@ -5,20 +11,16 @@ function initAll ($http, $scope){
 	if($scope.pagePath.last() && $scope.pagePath.last().length==0) $scope.pagePath.pop();
 	if('code' == $scope.pagePath.last()){
 		console.log('----initAll------code---------');
-		$scope.icpc2Laguage = function(lg){
-			var url = '/f/config/icpc2/ICPC2-'+lg+'.json';
-			console.log(url);
-			$http.get(url).then(
-				function(response) {
-					$scope.icpc = response.data;
-					console.log($scope.icpc);
-				}, function(response) {
-					console.error(response);
-				}
-			);
+//		$scope.seekParam = '';
+		$scope.isICPCCodeInSeek = function(k2,v2){
+			var isInSeek = true;
+			if(k2){
+				isInSeek
+					= v2.toLowerCase().indexOf($scope.icpc.seekParam.toLowerCase())>=0 
+					|| k2.toLowerCase().indexOf($scope.icpc.seekParam.toLowerCase())>=0;
+			}
+			return isInSeek;
 		}
-//		$scope.icpc2Laguage('en');
-		$scope.icpc2Laguage('ua');
 		$scope.getColor = function(key){
 			var color = 'green';
 			angular.forEach($scope.icpc.color, function(value, colorKey) {
@@ -28,6 +30,56 @@ function initAll ($http, $scope){
 			});
 			return color;
 		}
+		$scope.isShowPartColor = function(k2){
+			var isInSeek = true;
+			if($scope.icpc.showPartColor){
+				var color = $scope.getColor(k2);
+				if(!$scope.icpc.color[color].showPartColor)
+					isInSeek = false;
+				else
+					isInSeek = $scope.icpc.color[color].showPartColor;
+			}
+			return isInSeek;
+		}
+		$scope.clickShowPartColor = function(color){
+			console.log(color + '-showPartColor-'+$scope.icpc.showPartColor);
+			if(color){
+				var isOneColorPart = false;
+				angular.forEach($scope.icpc.color, function(value, colorKey) {
+					if(value.showPartColor){
+						isOneColorPart = true;
+					}
+				});
+				$scope.icpc.showPartColor = isOneColorPart;
+			}else{
+				if(!$scope.icpc.showPartColor){
+					angular.forEach($scope.icpc.color, function(value, colorKey) {
+						value.showPartColor = false;
+					});
+				}
+			}
+		}
+		$scope.containsSeekParam = function(o){
+			if(o && o.name)
+			return o.name.indexOf($scope.seekParam);
+		}
+		$scope.icpc2Laguage = function(lg){
+			var url = '/f/config/icpc2/ICPC2-'+lg+'.json';
+			console.log(url);
+			$http.get(url).then(
+				function(response) {
+					$scope.icpc = response.data;
+					$scope.icpc.seekParam = '';
+					$scope.icpc.expandAll = true;
+					expandAll($scope.icpc, $scope.icpc.group);
+					console.log($scope.icpc);
+				}, function(response) {
+					console.error(response);
+				}
+			);
+		}
+//		$scope.icpc2Laguage('en');
+		$scope.icpc2Laguage('ua');
 	}
 
 	$http.get('/f/config/mvp1.algoritmed.meddoc.config.json').then(
