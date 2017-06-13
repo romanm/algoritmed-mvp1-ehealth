b2771e24bbd55f1e268e58b0eeb4be3f794b9b1e
roman.mishchenko@gmail.com <roman.mishchenko@gmail.com>
2017-06-13 09:41:06 +0300

    test2_api__legal_entities problem add2

--- install-am-medic.sh
+++ install-am-medic.sh
@@ -1,7 +1,10 @@
 cd /home/roman/algoritmed2.com/algoritmed-mvp1-ehealth
 gradlew clean
 rm -r bin/
-tar czf ../algoritmed-mvp1-ehealth.tar.gz ../algoritmed-mvp1-ehealth
-scp ../algoritmed-mvp1-ehealth.tar.gz holweb@178.20.157.117:/home/holweb/server-medic
+cd ..
+rm algoritmed-mvp1-ehealth.tar.gz
+tar czf algoritmed-mvp1-ehealth.tar.gz algoritmed-mvp1-ehealth
+scp algoritmed-mvp1-ehealth.tar.gz holweb@178.20.157.117:/home/holweb/server-medic
 ssh -t holweb@178.20.157.117 "cd ~/server-medic/; bash; echo ./restart-am-medic.sh"
+cd /home/roman/algoritmed2.com/algoritmed-mvp1-ehealth
 
