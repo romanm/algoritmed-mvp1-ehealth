45551ceb6e50137bff0da84dceacdff4c03b1f1b
roman.mishchenko@gmail.com <roman.mishchenko@gmail.com>
2017-11-07 08:01:13 +0200

    x

--- src/main/java/org/algoritmed/mvp1/WebSecurityConfig.java
+++ src/main/java/org/algoritmed/mvp1/WebSecurityConfig.java
@@ -3,10 +3,13 @@ package org.algoritmed.mvp1;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /**
@@ -18,6 +21,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Bean
+	PasswordEncoder passwordEncoder() {
+	    return NoOpPasswordEncoder.getInstance();
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
