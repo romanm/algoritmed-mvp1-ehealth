7a1e42862c834d0394836c999aab2dc473a89d73
roman.mishchenko@gmail.com <roman.mishchenko@gmail.com>
2017-11-09 00:21:08 +0200

    d

---
+++ src/main/java/org/algoritmed/mvp1/medic/OAuthRestCommon.java
@@ -0,0 +1,42 @@
+package org.algoritmed.mvp1.medic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class OAuthRestCommon {
+	@Autowired ObjectMapper mapper = new ObjectMapper();
+	String uri = "https://api.ehealth.world/oauth/tokens/";
+	protected String getBodyForOAuthTokenRequest(String code) {
+		Map oauth_tokenMap = getBodyMapForOAuthTokenRequest(code);
+		String oauth_tokens_body = "";
+		try {
+			oauth_tokens_body = mapper.writeValueAsString(oauth_tokenMap);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return oauth_tokens_body;
+	}
+
+	protected Map getBodyMapForOAuthTokenRequest(String code) {
+		Map oauth_tokenMap = new HashMap<>();
+		Map tokenMap = new HashMap<>();
+		tokenMap.put("grant_type", "authorization_code");
+		tokenMap.put("code", code);
+		tokenMap.put("client_id", "bf48fba2-e4e8-4a06-aeaa-345d8346d7bb");
+		tokenMap.put("client_secret", "ZHdqTTVGWjYrMFRRa0hoYmpGVTFldz09");
+		tokenMap.put("redirect_uri", "https://medic.algoritmed.com/r/from_oauth_tokens");
+		tokenMap.put("scope", "employee:read employee:write"
+				+ " employee_request:approve employee_request:read employee_request:write employee_request:reject"
+				+ " legal_entity:read"
+				+ " division:read division:write"
+				+ " declaration_request:write declaration_request:read employee:deactivate"
+				+ " otp:read otp:write ");
+		oauth_tokenMap.put("token", tokenMap);
+		return oauth_tokenMap;
+	}
+}
