9414ff82a971286af8449104f257e0e871cc4e6f
roman.mishchenko@gmail.com <roman.mishchenko@gmail.com>
2017-11-08 19:46:11 +0200

    y

--- src/main/java/org/algoritmed/mvp1/medic/OAuthRest.java
+++ src/main/java/org/algoritmed/mvp1/medic/OAuthRest.java
@@ -57,8 +57,8 @@ public class OAuthRest  extends DbAlgoritmed {
 		Response response = client.target(uri)
 				.request(MediaType.APPLICATION_JSON_TYPE)
 				.header("cache-control", "no-cache")
+				.header("postman-token", "560ff187-848c-467a-d1b5-d4383ecfa911")
 				.post(payload);
-//				.header("postman-token", "560ff187-848c-467a-d1b5-d4383ecfa911")
 //				.header("content-type", "application/json")
 
 				System.out.println("status: " + response.getStatus());
