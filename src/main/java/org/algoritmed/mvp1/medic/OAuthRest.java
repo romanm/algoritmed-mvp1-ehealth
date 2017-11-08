a20e5829d8887a73bed5c33adb5e2267340d8336
roman.mishchenko@gmail.com <roman.mishchenko@gmail.com>
2017-11-08 21:03:51 +0200

    y

--- src/main/java/org/algoritmed/mvp1/medic/OAuthRest.java
+++ src/main/java/org/algoritmed/mvp1/medic/OAuthRest.java
@@ -1,6 +1,7 @@
 package org.algoritmed.mvp1.medic;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,10 +14,14 @@ import javax.ws.rs.core.Response;
 import org.algoritmed.mvp1.DbAlgoritmed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class OAuthRest  extends DbAlgoritmed {
@@ -30,40 +35,65 @@ public class OAuthRest  extends DbAlgoritmed {
 				);
 		return "redirect:/v/admin-msp";
 	}
+	@Autowired ObjectMapper mapper = new ObjectMapper();
 	@GetMapping(value = "/r/to_oauth_tokens")
 	public String  to_oauth_tokens(@RequestParam("code") String code){
+//		String oauth_tokens_body = "{  'token': "
+//				+ "{"
+//				+ "    'grant_type': 'authorization_code'"
+//				+ ",    'code': '"+ code+ "'"
+//				+ ",    'client_id': 'bf48fba2-e4e8-4a06-aeaa-345d8346d7bb'"
+//				+ ",    'client_secret': 'ZHdqTTVGWjYrMFRRa0hoYmpGVTFldz09'"
+//				+ ",    'redirect_uri': 'https://medic.algoritmed.com/r/from_oauth_tokens'"
+//				+ ",    'scope': "
+//				+ "'employee:read employee:write"
+//				+ " employee_request:approve employee_request:read employee_request:write employee_request:reject"
+//				+ " legal_entity:read"
+//				+ " division:read division:write"
+//				+ " declaration_request:write declaration_request:read employee:deactivate"
+//				+ " otp:read otp:write' "
+//				+ "}}";
 		Client client = ClientBuilder.newClient();
-		String oauth_tokens_body = "{  'token': "
-				+ "{"
-				+ "    'grant_type': 'authorization_code'"
-				+ ",    'code': '"+ code+ "'"
-				+ ",    'client_id': 'bf48fba2-e4e8-4a06-aeaa-345d8346d7bb'"
-				+ ",    'client_secret': 'ZHdqTTVGWjYrMFRRa0hoYmpGVTFldz09'"
-				+ ",    'redirect_uri': 'https://medic.algoritmed.com/r/from_oauth_tokens'"
-				+ ",    'scope': 'employee:read employee:write"
+		Map oauth_tokenMap = new HashMap<>();
+		Map tokenMap = new HashMap<>();
+		tokenMap.put("grant_type", "authorization_code");
+		tokenMap.put("code", code);
+		tokenMap.put("client_id", "bf48fba2-e4e8-4a06-aeaa-345d8346d7bb");
+		tokenMap.put("client_secret", "ZHdqTTVGWjYrMFRRa0hoYmpGVTFldz09");
+		tokenMap.put("redirect_uri", "https://medic.algoritmed.com/r/from_oauth_tokens");
+		tokenMap.put("scope", "employee:read employee:write"
 				+ " employee_request:approve employee_request:read employee_request:write employee_request:reject"
 				+ " legal_entity:read"
 				+ " division:read division:write"
 				+ " declaration_request:write declaration_request:read employee:deactivate"
-				+ " otp:read otp:write' "
-				+ "}}";
-		System.err.println(oauth_tokens_body);
-		Entity payload = Entity.json(oauth_tokens_body);
-		String server = env.getProperty("config.uri_oauth2");
-		String uri = server	+ "/oauth/tokens";
-		System.err.println(uri);
-		uri = "https://api.ehealth.world/oauth/tokens/";
-		System.err.println(uri);
-		Response response = client.target(uri)
-				.request(MediaType.APPLICATION_JSON_TYPE)
-				.header("cache-control", "no-cache")
-				.header("postman-token", "560ff187-848c-467a-d1b5-d4383ecfa911")
-				.post(payload);
+				+ " otp:read otp:write ");
+		oauth_tokenMap.put("token", tokenMap);
+		try {
+			String oauth_tokens_body = mapper.writeValueAsString(oauth_tokenMap);
+			System.err.println("oauth_tokens_body");
+			System.err.println(oauth_tokens_body);
+			System.err.println("oauth_tokens_body END");
+			Entity payload = Entity.json(oauth_tokens_body);
+			System.err.println("payload");
+			System.err.println(payload);
+//			String server = env.getProperty("config.uri_oauth2");
+//			String uri = server	+ "/oauth/tokens";
+//			System.err.println(uri);
+			String uri = "https://api.ehealth.world/oauth/tokens/";
+			System.err.println(uri);
+			Response response = client.target(uri)
+					.request(MediaType.APPLICATION_JSON_TYPE)
+					.header("cache-control", "no-cache")
+//			.header("postman-token", "560ff187-848c-467a-d1b5-d4383ecfa911")
+//			.header("Authorization", "Bearer c490c936651a0f6badeb426721076437");
+					.post(payload);
 //				.header("content-type", "application/json")
-
-				System.out.println("status: " + response.getStatus());
-				System.out.println("headers: " + response.getHeaders());
-				System.out.println("body:" + response.readEntity(String.class));
+			System.out.println("status: " + response.getStatus());
+			System.out.println("headers: " + response.getHeaders());
+			System.out.println("body:" + response.readEntity(String.class));
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
 		return "redirect:/v/admin-msp";
 	}
 
