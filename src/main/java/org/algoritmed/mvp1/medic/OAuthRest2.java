8b33e8090d3d022fdec6331da5474843731e7d02
roman.mishchenko@gmail.com <roman.mishchenko@gmail.com>
2017-11-09 00:32:29 +0200

    d

--- src/main/java/org/algoritmed/mvp1/medic/OAuthRest2.java
+++ src/main/java/org/algoritmed/mvp1/medic/OAuthRest2.java
@@ -8,12 +8,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+@Controller
 public class OAuthRest2 extends OAuthRestCommon{
 	@Autowired ObjectMapper mapper = new ObjectMapper();
 	@GetMapping(value = "/r/to_oauth_tokens2")
