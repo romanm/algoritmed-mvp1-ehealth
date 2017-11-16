c81986678cfc85eb5bf9ff4edc79eba927e12905
roman.mishchenko@gmail.com <roman.mishchenko@gmail.com>
2017-11-16 23:07:14 +0200

    b

--- src/main/java/org/algoritmed/mvp1/medic/OAuthRest2.java
+++ src/main/java/org/algoritmed/mvp1/medic/OAuthRest2.java
@@ -8,34 +8,39 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@RestController
+//@RestController
+@Controller
 @RequestMapping(value = "${config.security_prefix}")
 public class OAuthRest2 extends OAuthRestCommon{
 	@Autowired ObjectMapper mapper = new ObjectMapper();
 	@GetMapping(value = "/to_oauth_tokens")
 	public String  to_oauth_tokens(@RequestParam("code") String code, HttpServletResponse response){
 		logger.info("---------------\n"
-				+ "/from_oauth_tokens2"
+				+ "/to_oauth_tokens"
 				+ "\n" 
 				+ "\n" +response
+				+ "\n ------------------" 
 				);
 		Map bodyMapForOAuthTokenRequest = getBodyMapForOAuthTokenRequest(code);
 		
 		String oauth_tokens_body = mapToString(bodyMapForOAuthTokenRequest);
 		System.err.println("---------------");
+		System.err.println("oauth_tokens_body");
 		System.err.println(oauth_tokens_body);
 //		testCURL(oauth_tokens_body);
 		
 		HttpHeaders headers = getRestTemplateHeader();
+		System.err.println("headers");
 	    System.err.println(headers);
+	    System.err.println("uri_oauth2_code_grant");
 	    System.err.println(uri_oauth2_code_grant);
 	    System.err.println(31);
 	    System.err.println("bodyMapForOAuthTokenRequest");
