b35753a71aefe7aae418692bf1ec38f0dd06b94e
romanm <roman.mishchenko@gmail.com>
2017-11-22 00:22:34 +0200

    j

--- src/main/java/org/algoritmed/mvp1/medic/OAuthRest2.java
+++ src/main/java/org/algoritmed/mvp1/medic/OAuthRest2.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
@@ -25,8 +26,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @RequestMapping(value = "${config.security_prefix}")
 public class OAuthRest2 extends OAuthRestCommon{
 	@Autowired ObjectMapper mapper = new ObjectMapper();
-	@GetMapping(value = "/to_oauth_tokens")
-	public String  to_oauth_tokens(@RequestParam("code") String code, HttpServletResponse response){
+	@GetMapping(value = "/to_oauth_tokens_{{msp_id}}")
+	public String  to_oauth_tokens(@RequestParam("code") String code, @PathVariable String msp_id, HttpServletResponse response){
 		logger.info("\n ------29--+++---------\n"
 				+ "/to_oauth_tokens"
 				+ "\n" 
